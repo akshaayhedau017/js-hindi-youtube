d784f452029aa6b1ba7c33d2e11f6549387ecbc3
akshayy_10 <107067673+akshayhedau69@users.noreply.github.com>
2024-04-06 09:39:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"//treat all JS code as newer version 
+//alert(3+3) we are using JS ,not browser
+
+console.log(3+3);
+// code readiblity should be high
+
+
+let name="akshay"
+let age=21
+
+console.log(typeof null);
